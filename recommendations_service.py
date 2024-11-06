5c95b172cf51a91949acff59a472519a882c8c59
Михаил Горбунов <ujh@mail.ru>
2024-11-06 08:46:33 +0000

    root dir part2 ranked

--- recommendations_service.py
+++ recommendations_service.py
@@ -4,10 +4,12 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from recsys_hendler import RecSysHeandler
 from events_store import EventStore
+from rec_ranks import RecRanks
 import random
 
 rec_store = RecSysHeandler()
 events_store = EventStore()
+rec_rank = RecRanks()
 
 #logging.basicConfig(filename='./service/logs/fastapi.log',level=logging.INFO)
 
@@ -30,7 +32,7 @@ async def recommendations(user_id: int, k: int = 100):
     """
     # вернёт k персональных рекомендаций если они подготовлены offline
     # или k популярных треков
-    personal = rec_store.personal_rec(user_id,k)
+    personal = rec_store.personal_rec(user_id,k*3)
     events = events_store.get(user_id, 10)
     if len(events) == 0:
         # отдаём рекомендации для пользователя без истории
@@ -43,10 +45,14 @@ async def recommendations(user_id: int, k: int = 100):
         # убираем из рекомендаций треки, которые пользователь слушал и дубли от персональных/популярных
         recs_online = list(set(recs_online) - set(events) - set(personal))
         # поскольку score для треков посчитан как степень похожести на каждый из отдельных item_id 
-        # и ни как не сопостамваим для разных треков, по ранжировать их между собой можно либо случайным образом
+        # и ни как не сопостамваим для разных треков, то ранжировать их между собой можно либо случайным образом
         # либо взвешивать треки на жанровый, альбомный и исполнительский рейтинг для каждого пользователя
         # такая реализация вполне адекватна, но может работать медленно. т.к. взаимодействуетс большими таблицами events и items
-        recs = random.sample(recs_online + personal,k=k)
+        
+        all_recs_for_rank = recs_online + personal
+        
+        #recs = random.sample(all_recs_for_rank,k=k)
+        recs = rec_rank.calc_ranks(user_id,all_recs_for_rank)[:k]
 
     return {"recs": recs}
 
